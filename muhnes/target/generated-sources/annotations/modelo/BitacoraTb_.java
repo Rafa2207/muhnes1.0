14f8c3795bd0ef32e77ac6927d1cf3a6326e41c6
franklordark90 <frank_18_2007@hotmail.com>
2016-04-01 19:45:32 -0600

    Corrección de ortografía en la ayuda y restricción en la ayuda del usuario digitador. Faltaria ayuda de crear back up.

--- muhnes/target/generated-sources/annotations/modelo/BitacoraTb_.java
+++ muhnes/target/generated-sources/annotations/modelo/BitacoraTb_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.UsuarioTb;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-29T15:07:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-29T15:21:17")
 @StaticMetamodel(BitacoraTb.class)
 public class BitacoraTb_ { 
 
--- muhnes/target/generated-sources/annotations/modelo/InsumoTb_.java
+++ muhnes/target/generated-sources/annotations/modelo/InsumoTb_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.ActividadTb;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-29T15:07:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-29T15:21:17")
 @StaticMetamodel(InsumoTb.class)
 public class InsumoTb_ { 
 
